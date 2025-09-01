11d28c873732dc204f2c67eac10bc0b638f4adba
James Quessy <james@quessy.com>
2025-09-01 16:12:10 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <div className="hidden md:flex items-center justify-between">
               <Link href="/" className="flex items-center gap-2">
                 <Image
-                  src="/propvisions_logo.png"
+                  src="/propvisions_logo_no_text.png"
                   alt="PropVisions logo"
                   width={96}
                   height={24}
